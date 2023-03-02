1ee91e2bae4a5768655cbdeb013ea4891567cbc8
quingsley <quingsley@hotmail.com>
2023-03-02 18:29:27 +0300

    Add spinner and error handling for fetching meals

--- src/Components/Meals/AvailableMeals.tsx
+++ src/Components/Meals/AvailableMeals.tsx
@@ -1,34 +1,62 @@
 import Card from "../UI/Card";
+import Spinner from "../UI/Spinner";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+import { useEffect, useState } from "react";
+
+interface MealObj {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+}
+
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState<Array<MealObj>>([] as Array<MealObj>);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState({
+    message: "Something went wrong",
+    hasError: false,
+  });
+  useEffect(() => {
+    const fetchMeals = async () => {
+      try {
+        const response = await fetch(
+          "https://elimisha-c5ce8-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
+        );
+
+        if (!response.ok) {
+          setIsLoading(false);
+          throw new Error("Something went wrong!");
+        }
+        const fetchedMeals = await response.json();
+        setMeals(fetchedMeals);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+        setIsLoading(false);
+        setHttpError({ message: "Something went wrong", hasError: true });
+      }
+    };
+    fetchMeals();
+  }, []);
+  if (isLoading) {
+    return (
+      <div className={classes["spinner-box"]}>
+        <Spinner />
+      </div>
+    );
+  }
+
+  if (httpError.hasError) {
+    return (
+      <div className={classes["spinner-box"]}>
+        <h1 className={classes.error}>{httpError.message}</h1>
+      </div>
+    );
+  }
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
       name={meal.name}
@@ -37,6 +65,7 @@ const AvailableMeals = () => {
       id={meal.id}
     />
   ));
+
   return (
     <section className={classes.meals}>
       <Card>
